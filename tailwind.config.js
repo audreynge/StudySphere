b06a894f5c024ef0c556f465071f94c2c558959b
Audrey Ng <audrey518@gmail.com>
2024-03-29 01:40:22 -0700

    home page mostly finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins'],
+      },
+      colors: {
+          'white': '#fff',
+          'black': '#000',
+          'SS-dark-gray': '#BABABA',
+          'SS-purple': '#BD74C9',
+          'SS-green': '#88B28C',
+          'SS-lightgreen': '#DEFFDF',
+          'SS-lightestgreen': '#EAFFE8',
+          'SS-black': '#202020',
+          'SS-gray': '#E0DADA',
+      },
+    },
   },
   plugins: [],
 }
